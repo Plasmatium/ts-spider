604fcf832335f01e3bf4e714e8844dcf4e20e6a3
Plasmatium <slv309@163.com>
2017-11-20 17:51:34 +0800

    progress: fetchProductUrlsOnPage

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var axios_1 = require("axios");
-var config = {};
+const axios_1 = require("axios");
+const cheerio = require("cheerio");
+let config = {};
 config.method = 'get';
 config.baseURL = 'http://www.emerson.com/en-us/catalog/liquid-analysis';
-var rslt;
-axios_1.default('', config).then(function (res) {
-    rslt = res;
-    console.log('done');
-});
+let url = '?fetchFacets=true#facet:&facetLimit:&productBeginIndex:0&orderBy:&pageView:grid&minPrice:&maxPrice:&pageSize:&';
+let $;
+const fetchProductUrlsOnPage = async (pageUrl) => {
+    let res = await axios_1.default(pageUrl, config);
+    $ = cheerio.load(res.data);
+    let anchors = $('.product_name a');
+    let productUrls = [].map.call(anchors, (a) => {
+        return a.attribs.href;
+    });
+    debugger;
+    return productUrls;
+};
+(async () => {
+    await fetchProductUrlsOnPage(url);
+})().then(() => console.log('done!'));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
 import axios, { AxiosRequestConfig } from 'axios'
+import * as cheerio from 'cheerio'
 
 let config: AxiosRequestConfig = {}
 
 config.method = 'get'
 config.baseURL = 'http://www.emerson.com/en-us/catalog/liquid-analysis'
+let url = '?fetchFacets=true#facet:&facetLimit:&productBeginIndex:0&orderBy:&pageView:grid&minPrice:&maxPrice:&pageSize:&'
 
-let rslt: any
-axios('', config).then(res => {
-  rslt = res
-  console.log('done')
-})
+let $: any
+
+const fetchProductUrlsOnPage = async (pageUrl: string) => {
+  let res = await axios(pageUrl, config)
+  $ = cheerio.load(res.data)
+  let anchors = $('.product_name a')
+  let productUrls = [].map.call(anchors, (a: CheerioElement) => {
+    return a.attribs.href
+  })
+  debugger
+  return productUrls
+}
+
+(async () => {
+  await fetchProductUrlsOnPage(url)
+})().then(() => console.log('done!'))
